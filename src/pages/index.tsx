c0709a9cabe75e60e65f13101487f55d32407dde
Alejandro Sosa <agsosa74@gmail.com>
2021-08-12 14:47:06 -0300

    Fix styled-components ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import issuesService from '../services/issue.service';
 import styled from 'styled-components';
 import Issue from '../types/issue.interface';
 import IssueCard from '../components/IssueCard';
 
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.silver};
+`;
+
 const IssuesContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 5rem;
+  background-color: ${(props) => props.theme.silver};
 `;
 
 const Home: NextPage = () => {
@@ -30,13 +37,13 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div>
+    <PageContainer>
       <IssuesContainer>
         {data.map((d) => (
           <IssueCard key={d.issueId} issue={d} />
         ))}
       </IssuesContainer>
-    </div>
+    </PageContainer>
   );
 };
 
