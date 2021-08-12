2fd27da1efa97db52dea830eea434f8510f08496
Alejandro Sosa <agsosa74@gmail.com>
2021-08-12 18:41:11 -0300

    Update IssueCard hover style

--- src/components/issues/IssueCard.tsx
+++ src/components/issues/IssueCard.tsx
@@ -11,6 +11,12 @@ const Container = styled.div`
   margin-bottom: 1rem;
   padding: 1rem 2rem;
   font-size: 1.5rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  :hover {
+    background-color: rgb(0, 0, 0, 0.05);
+  }
 `;
 
 interface PropTypes {
