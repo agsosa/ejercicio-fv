65829c459d9ac063d3bbca0377fd0a4fc4b1dd6f
Alejandro Sosa <agsosa74@gmail.com>
2021-08-13 10:00:25 -0300

    Fix composer service types

--- src/services/composer.service.ts
+++ src/services/composer.service.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Issue from '../types/issue.interface';
+import Order from '../types/order.interface';
 
 const http = axios.create({
   baseURL: 'https://mocki.io/',
@@ -7,7 +7,7 @@ const http = axios.create({
 
 class ComposerService {
   async findOne(orderId: string, productGroupingType: string, issueId: string) {
-    return http.get<Issue[]>('/v1/86dd4882-29f8-4607-ad10-f388b4d1f009'); // TODO: Cambiar a post endpoint real cuando tenga acceso al VPN
+    return http.get<Order>('/v1/86dd4882-29f8-4607-ad10-f388b4d1f009'); // TODO: Cambiar a post endpoint real cuando tenga acceso al VPN
   }
 }
 
